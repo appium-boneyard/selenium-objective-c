1b058e92ef759145ab7e884df7b41ff7bf1eb4bb
penguinho <penguinho@aol.com>
2015-01-23 17:24:35 +0000

    Adding umbrella headers

--- Selenium/Selenium/Selenium.h
+++ Selenium/Selenium/Selenium.h
@@ -1,19 +1,3 @@
-//
-//  SeleniumForiOS.h
-//  SeleniumForiOS
-//
-//  Created by Dan Cuellar on 28/10/2014.
-//  Copyright (c) 2014 Appium. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
-
-//! Project version number for SeleniumForiOS.
-FOUNDATION_EXPORT double SeleniumForiOSVersionNumber;
-
-//! Project version string for SeleniumForiOS.
-FOUNDATION_EXPORT const unsigned char SeleniumForiOSVersionString[];
-
 #import <Foundation/Foundation.h>
 
 #import <Selenium/SEBy.h>
